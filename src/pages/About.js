26df5f943c3ecd61a0463969f2bc92878d238484
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-24 18:41:26 -0500

    About page content added and styled. Copy proofed.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,44 @@
 import React from "react";
+import Hero from "../components/Hero"
+import portrait from "./assets/mn_portrait.jpg"
+import "./css/style.css"
 
 function About() {
   return (
     <div>
-      <section className="row">
-        <article className="card portrait-info col-sm-12 my-5">
-          <h1 className="card-header text-white font-weight-bold">Hello, I'm James Wheeler</h1>
-                <hr className="rule-lines"></hr>
-                <h2 class="subhead col-sm-8">
-                        Citizen coder with a <span>Johns Hopkins</span> education currently coding at <span>Stanley
-                            Black and Decker</span>
-                </h2>
-
-                <p>
-                        <a href="https://www.linkedin.com/in/james-w-54270740/" class="contact-list"
-                            target="_blank">LINKED IN</a>
-                        <a> / </a>
-                        <a href="https://github.com/jrtwheeler" class="contact-list" target="_blank"> GITHUB</a>
-                        <a> / </a>
-                        <a href="https://docs.google.com/document/d/e/2PACX-1vTRm2WKIL8udaUOcZXCVm3yrinhqizl94i95SiFVBGaXU1iZIsq3B1RHFCroQ2Mg8V0x-Z4O9ex0TwF/pub"
-                            class="contact-list" target="_blank"> RESUME </a>
-                    </p>
-          </article>
-      </section>
+      <Hero>
+        <h1>About Me</h1>
+        <img id="portrait" className="float-left mr-2 mb-2" src={portrait}/>
+        <p>
+          Front-end web developer leveraging design and writing background to build a more intuitive user experience
+          on the web. Certified in full stack development from the University of Johns Hopkins, with experience in
+          JavaScript, CSS, HTML, JQUERY and responsive web design. Citizen coder who designed automations in
+          Javascript and Python for Adobe stack for Stanley Black and Decker that saved the company forty weeks of
+          manual data entry a year.
+          </p>
+        <p>
+          My automated layout system merited an Eagle Award for SBD employee of the year. To
+          build the system, I taught myself Javascript and Python on weekends. I applied agile design to develop the
+          system. I improve efficiency. I have fifteen years of supervisor experience. I am a committed lifelong
+          learner who is willing to educate myself through any available means. Experienced in meeting and beating
+          deadlines.
+          </p>
+        <hr />
+        <h5 class="contact-about">CONTACT ME</h5>
+        <span class="badge badge-light">(410) 302 1006</span>
+        <span class="badge badge-light">wheelewell@yahoo</span>
+        <hr class="rule-lines" />
+        <p>
+          <a href="https://www.linkedin.com/in/james-w-54270740/" className="contact-list"
+            target="_blank">LINKED IN</a>
+          <a> / </a>
+          <a href="https://github.com/jrtwheeler" className="contact-list" target="_blank"> GITHUB</a>
+          <a> / </a>
+          <a href="https://docs.google.com/document/d/e/2PACX-1vTRm2WKIL8udaUOcZXCVm3yrinhqizl94i95SiFVBGaXU1iZIsq3B1RHFCroQ2Mg8V0x-Z4O9ex0TwF/pub"
+            className="contact-list" target="_blank"> RESUME </a>
+        </p>
+        <hr class="hr-contacts" />
+      </Hero>
     </div>
   );
 }
