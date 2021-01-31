fef9c3826230fb319d754c6ad159d41772975574
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-31 18:41:42 -0500

    The url element on the Home page was not working so I replaced it with the hard coded url.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Hero from "../components/Hero"
+import Hero from "../components/Hero";
+import API from "../utils/github.json";
 
 function Home() {
   return (
     <div>
       <Hero className="mb-0">
-        <h1>Hello, I'm Jim Wheeler.</h1>
+        <h1>Hello, I'm {API.basics.name}.</h1>
         <h2>
           Citizen coder with a <span>Johns Hopkins</span> education currently coding at <span>Stanley Black and Decker</span>.
         </h2>
@@ -15,7 +16,7 @@ function Home() {
           <a href="https://www.linkedin.com/in/james-w-54270740/" className="contact-list"
             target="_blank">LINKED IN</a>
           <a> / </a>
-          <a href="https://github.com/jrtwheeler" className="contact-list" target="_blank"> GITHUB</a>
+          <a href={"https://github.com/jrtwheeler"} className="contact-list" target="_blank"> GITHUB</a>
           <a> / </a>
           <a href="https://docs.google.com/document/d/e/2PACX-1vTRm2WKIL8udaUOcZXCVm3yrinhqizl94i95SiFVBGaXU1iZIsq3B1RHFCroQ2Mg8V0x-Z4O9ex0TwF/pub"
             className="contact-list" target="_blank"> RESUME </a>
