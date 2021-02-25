f5443189fe8c6d463264d380639724470e40555a
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-02-24 20:35:25 -0500

    Added font awesome file ison in place of the word resume in the nav bar

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navbar, Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile, faCoffee } from '@fortawesome/fontawesome-free-solid'
+;
 
 
 // Depending on the current path, this component sets the "active" className on the appropriate navigation link item
@@ -23,7 +25,8 @@ function NavigationBar() {
           <FontAwesomeIcon icon={['fab', 'github']} /></h2>
           </Nav.Link>
           <Nav.Link href="https://docs.google.com/document/d/e/2PACX-1vTRm2WKIL8udaUOcZXCVm3yrinhqizl94i95SiFVBGaXU1iZIsq3B1RHFCroQ2Mg8V0x-Z4O9ex0TwF/pub" target="_blank">
-            Resume
+          <h2>
+          <FontAwesomeIcon icon={faFile} /></h2> 
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
