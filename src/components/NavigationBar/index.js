980fa7007314bef33011eeda3b72245dee3922c3
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-02-27 23:56:41 -0500

    I added a tooltip to the resume icon.

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -41,10 +41,19 @@ function NavigationBar() {
           >
             See more on GitHub
             </ReactTooltip>
-          <Nav.Link href="https://docs.google.com/document/d/e/2PACX-1vTRm2WKIL8udaUOcZXCVm3yrinhqizl94i95SiFVBGaXU1iZIsq3B1RHFCroQ2Mg8V0x-Z4O9ex0TwF/pub" target="_blank">
+          <Nav.Link href="https://docs.google.com/document/d/e/2PACX-1vTRm2WKIL8udaUOcZXCVm3yrinhqizl94i95SiFVBGaXU1iZIsq3B1RHFCroQ2Mg8V0x-Z4O9ex0TwF/pub" target="_blank" data-tip
+            data-for="Resume">
             <h2>
               <FontAwesomeIcon icon={faFile} /></h2>
           </Nav.Link>
+          <ReactTooltip
+            id="Resume"
+            place="bottom"
+            effect="solid"
+            backgroundColor="#4A6479"
+          >
+            See my resume
+            </ReactTooltip>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
