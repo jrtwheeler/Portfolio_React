1df86c9d1280d5fa894797ad4ebd9adb696fe0b2
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-03-02 22:05:55 -0500

    Fixed typo on portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -17,7 +17,7 @@ function Portfolio () {
         <h1>PORTFOLIO</h1>
         <p>
             I built these projects with a mixture of coffee, grit, Front-End Web Development, HTML 5, CSS 3, Bootstrap 4,
-            Javascript ES6, DOM Manipulation, jQuery, Bash Command Line, Git, GitHub and Version Control, Backend Web
+            JavaScript ES6, DOM Manipulation, jQuery, Bash Command Line, Git, GitHub and Version Control, Backend Web
             Development, Node.js, NPM, Express.js, EJS, REST, APIs, Databases, SQL, MongoDB, Mongoose, Authentication,
             Firebase, React.js, React Hooks, Web Design, Deployment with GitHub Pages, Heroku and MongoDB Atlas,
             Python3, Extendscript.
