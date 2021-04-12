b88294e526fe8b54da7c612642dcf363fe4dfdb2
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-04-11 23:29:42 -0400

    Added styled react tootips to the portfolio page

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,6 +3,7 @@ import API from "../../utils/github";
 // get our fontawesome imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FaLink } from "react-icons/fa";
+import ReactTooltip from "react-tooltip";
 
 function Card() {
     if (API) {
@@ -16,12 +17,33 @@ function Card() {
                         <p className="card-text"> {result.description} </p>
                     </div>
                     <div>
-                        <a href={result.githubUrl} rel="noreferrer" target="blank" className="btn btn-dark ml-3 mb-3 mr-1" data-toggle="tooltip" data-placement="top" title="Open GitHub Repo"><FontAwesomeIcon icon={['fab', 'github']} /></a>
-                        <a href={result.repositoryUrl} rel="noreferrer" target="blank" className="btn btn-dark mb-3 ml-1" data-toggle="tooltip" data-placement="top" title="Open Webpage"><FaLink/></a>
+                        <ReactTooltip
+                            id="gitHub"
+                            place="bottom"
+                            effect="solid"
+                            backgroundColor="#4A6479"
+                        >
+                            Open GitHub Repo
+                        </ReactTooltip>
+
+                        <ReactTooltip
+                            id="app"
+                            place="bottom"
+                            effect="solid"
+                            backgroundColor="#4A6479"
+                        >
+                            Open App
+                        </ReactTooltip>
+
+                        <a href={result.githubUrl} rel="noreferrer" target="blank" className="btn btn-dark ml-3 mb-3 mr-1" data-tip
+                            data-for="gitHub"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+
+                        <a href={result.repositoryUrl} rel="noreferrer" target="blank" className="btn btn-dark mb-3 ml-1" data-tip
+                            data-for="app"><FaLink /> </a>
                     </div>
                 </div>
             )
-            
+
         })
         return (
             <div className="row">
